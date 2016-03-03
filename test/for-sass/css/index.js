f0c8563bed9bc1391836d6cb3521103b32dbe39b
qianjun.yang <qianjun.yang@qunar.com>
2016-03-03 16:49:10 +0800

    修复scss错误

--- test/for-sass/css/index.js
+++ test/for-sass/css/index.js
@@ -2,6 +2,9 @@ module.exports.styles = require('react-native').StyleSheet.create({
     "fancybox": {
         "width": 400
     },
+    "common-class": {
+        "color": "red"
+    },
     "a": {
         "color": "red",
         "fontColor": "red"
