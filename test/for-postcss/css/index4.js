2cfa1e306457d94d2628a1ee1cdd1fb74ae31af9
qianjun.yang <qianjun.yang@qunar.com>
2016-03-14 18:14:02 +0800

    修复border使用的一个bug

---
+++ test/for-postcss/css/index4.js
@@ -0,0 +1,11 @@
+module.exports.styles = require('react-native').StyleSheet.create({
+    "a": {
+        "borderWidth": 1,
+        "borderStyle": "solid",
+        "borderColor": "#ccc",
+        "borderTopWidth": 0,
+        "borderRightWidth": 1,
+        "borderBottomWidth": 0,
+        "borderLeftWidth": 1
+    }
+});
